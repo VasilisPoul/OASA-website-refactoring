f45b3c1fe27742649a1619d721a509a7d9307417
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2019-12-29 23:58:30 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -2,8 +2,6 @@
 
 HTML/CSS by: Maria Karamina 
 
-PHP script by: Giorgos Koursiounis
-
 -->
 
 <!DOCTYPE html>
@@ -38,80 +36,6 @@ PHP script by: Giorgos Koursiounis
   </head>
   <body>
 
-  <?php
-
-  $servername = "localhost";
-  $username = "user";
-  $password = "password";
-  $dbname = "oasa";
-
-  $delays = "";
-  $strikes = "";
-  $planned_work = "";
-  $good_service = "";
-  $out_of_order = "";
-
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  if($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  //find delays
-  $sql = "SELECT l.name, c.colour FROM line_status ls, line l, colour c WHERE ls.idline_status = l.idline_status AND ls.status = \"Καθυστερήσεις\" AND l.idcolour = c.idcolour";
-  $result = $conn->query($sql);
-
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      $delays .= "<button type=\"button\" class=\"btn disabled\" style=\"margin: 2px;color:White;background-color:" . $row["colour"] . ";\">" . $row["name"] . "</button>";
-    }
-  }
-
-  //find strikes
-  $sql = "SELECT l.name, c.colour FROM line_status ls, line l, colour c WHERE ls.idline_status = l.idline_status AND ls.status = \"Απεργίες\" AND l.idcolour = c.idcolour";
-  $result = $conn->query($sql);
-
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      $strikes .= "<button type=\"button\" class=\"btn disabled\" style=\"margin: 2px;color:White;background-color:" . $row["colour"] . ";\">" . $row["name"] . "</button>";
-    }
-  }
-
-  //find planned work
-  $sql = "SELECT l.name, c.colour FROM line_status ls, line l, colour c WHERE ls.idline_status = l.idline_status AND ls.status = \"Έργα σε εξέλιξη\" AND l.idcolour = c.idcolour";
-  $result = $conn->query($sql);
-
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      $planned_work .= "<button type=\"button\" class=\"btn disabled\" style=\"margin: 2px;color:White;background-color:" . $row["colour"] . ";\">" . $row["name"] . "</button>";
-    }
-  }
-
-  //find good service
-  $sql = "SELECT l.name, c.colour FROM line_status ls, line l, colour c WHERE ls.idline_status = l.idline_status AND ls.status = \"Ομαλή λειτουργία\" AND l.idcolour = c.idcolour";
-  $result = $conn->query($sql);
-
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      $good_service .= "<button type=\"button\" class=\"btn disabled\" style=\"margin: 2px;color:White;background-color:" . $row["colour"] . ";\">" . $row["name"] . "</button>";
-    }
-  }
-
-  //find planned work
-  $sql = "SELECT l.name, c.colour FROM line_status ls, line l, colour c WHERE ls.idline_status = l.idline_status AND ls.status = \"Εκτός λειτουργίας\" AND l.idcolour = c.idcolour";
-  $result = $conn->query($sql);
-
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      $out_of_order .= "<button type=\"button\" class=\"btn disabled\" style=\"margin: 2px;color:White;background-color:" . $row["colour"] . ";\">" . $row["name"] . "</button>";
-    }
-  }
-
-  $conn->close();
-  ?> 
-
-
-    
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
 	      <a href="index.html"><img src="images/oasa_logo_transparent.png" alt="logo" width="25%"></a>
@@ -211,6 +135,8 @@ PHP script by: Giorgos Koursiounis
      <section class="ftco-section ftco-no-pt bg-light">
     	<div class="container">
     		<h1 class="text-right">Κατάσταση Γραμμών</h1>
+
+		<span> <?php include 'service_status.php';?> </span>
 
     		<div class="light-box">
     			<div class="row justify-content-center">
