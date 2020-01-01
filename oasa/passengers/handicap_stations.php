3d9ea5bbd25de81d4478378ada8aba1a03036251
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2020-01-01 19:22:38 +0200

    Add files via upload

--- oasa/passengers/handicap_stations.php
+++ oasa/passengers/handicap_stations.php
@@ -5,18 +5,13 @@
 //
 
 session_start();
- 
-//if already logged in then redirect
-if(isset($_SESSION['loggedin'])){
-  echo $_SESSION['first_name'] . " " . $_SESSION['last_name'];
-}
 
 $servername = "localhost";
 $server_username = "user";
 $server_password = "password";
 $dbname = "oasa";
 
-$stations = "<table class="table"><thead><tr><th scope="col">#</th><th scope="col">Όνομα σταθμού/στάσης</th></tr></thead><tbody>";
+$stations = "<table class=\"table\"><thead><tr><th scope=\"col\">#</th><th scope=\"col\">Όνομα σταθμού/στάσης</th><th scope=\"col\">Περιοχή</th></tr></thead><tbody>";
  
 //create connection
 $conn = new mysqli($servername, $server_username, $server_password, $dbname);
@@ -24,13 +19,13 @@ if($conn->connect_error){
   die("Connection failed: " . $conn->connect_error);
 }
 
-//check if given username exists
+//find stations accessible by persons with disability
 $sql = "SELECT * FROM station s WHERE  disability_access = 1";
 $result = $conn->query($sql);
 
 if(!empty($result) && $result->num_rows > 0){
   while($row = $result->fetch_assoc()){
-    $stations .= "<tr><td>" . $row["idstation"] . "</td><td>" . $row["name"] . "</td></tr>";
+    $stations .= "<tr><td>" . $row["idstation"] . "</td><td>" . $row["name"] . "</td><td>" . $row["area"] . "</td></tr>";
   }
 }
 
