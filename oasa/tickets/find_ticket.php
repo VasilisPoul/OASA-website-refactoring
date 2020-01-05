997f8098f362bafb63f49e865e6012f99dde9e6d
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2020-01-05 19:58:34 +0200

    Add files via upload

--- oasa/tickets/find_ticket.php
+++ oasa/tickets/find_ticket.php
@@ -22,27 +22,38 @@ if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
     die("Connection failed: " . $conn->connect_error);
   }
 
-  //find ticket on database based on its id
-  $sql = "SELECT t.date, t.expired , tc.name AS tick_category_name, tc.price, uc.name AS user_cat_name FROM ticket t, ticket_category tc, user_category uc WHERE t.idticket = \"$idticket\" AND t.idticket_category = tc.idticket_category AND tc.iduser_category = uc.iduser_category";
-  $result = $conn->query($sql);
+  //check if username is given
+  if(empty($_POST["idticket"])){
+    $error = "Εισάγετε κωδικό εισιτηρίου";
+  } 
+  else{
+    $idticket = $_POST["idticket"];
+  }
 
-  if(!empty($result) && $result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      if($row["expired"] === 1){
+  if(empty($error)){
 
-        $date = $row["date"];
-        $tick_category_name = $row["tick_category_name"];
-        $price = $row["price"];
-        $user_cat_name = $row["user_cat_name"];
+    //find ticket on database based on its id
+    $sql = "SELECT t.date, t.expired , tc.name AS tick_category_name, tc.price, uc.name AS user_cat_name FROM ticket t, ticket_category tc, user_category uc WHERE t.idticket = $idticket AND t.idticket_category = tc.idticket_category AND tc.iduser_category = uc.iduser_category";
+    $result = $conn->query($sql);
 
-      }
-      else{
-        $error = "Δεν μπορείτε να επαναφορτίσετε εισιτήριο που δεν έχει λήξει";
+    if(!empty($result) && $result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        if($row["expired"] == 1){
+
+          $date = $row["date"];
+          $tick_category_name = $row["tick_category_name"];
+          $price = $row["price"];
+          $user_cat_name = $row["user_cat_name"];
+
+        }
+        else{
+          $error = "Δεν μπορείτε να επαναφορτίσετε εισιτήριο που δεν έχει λήξει";
+        }
       }
     }
-  }
-  else{
-    $error = "Το εισιτήριο δεν βρέθηκε";
+    else{
+      $error = "Το εισιτήριο δεν βρέθηκε";
+    }
   }
 
   $conn->close();
