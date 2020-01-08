1b6b80681a989c97b248e2210a3b21071cbd1c81
Maria <maria.karamina@gmail.com>
2020-01-08 20:29:02 +0200

    New urls

--- oasa/tickets/buy_card/buy_card.php
+++ oasa/tickets/buy_card/buy_card.php
@@ -10,54 +10,54 @@ HTML/CSS by: Maria Karamina (sdi1600059)
     <title>ΟΑΣΑ - Αγορά Εισιτηρίων</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="../images/favicon.ico" type="image/ico">
+    <link rel="icon" href="../../images/favicon.ico" type="image/ico">
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../../css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
     
-    <link rel="stylesheet" href="../css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <link rel="stylesheet" href="../../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../css/magnific-popup.css">
 
-    <link rel="stylesheet" href="../css/aos.css">
+    <link rel="stylesheet" href="../../css/aos.css">
 
-    <link rel="stylesheet" href="../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
 
-    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="../css/jquery.timepicker.css">
+    <link rel="stylesheet" href="../../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../../css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="../css/flaticon.css">
-    <link rel="stylesheet" href="../css/icomoon.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/additional.css">
+    <link rel="stylesheet" href="../../css/flaticon.css">
+    <link rel="stylesheet" href="../../css/icomoon.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/additional.css">
   </head>
   <body>
 
-    <?php include 'ticket_categories.php' ?>
+    <?php include '../ticket_categories.php' ?>
     
    <nav class="navbar navbar-expand-lg navbar-light ftco_navbar bg-dark ftco-navbar-light navbar-color" id="ftco-navbar">
       <div class="container">
-        <a href="../index.php"><img src="../images/oasa_logo_transparent.png" alt="logo" width="25%"></a>
+        <a href="../../index.php"><img src="../../images/oasa_logo_transparent.png" alt="logo" width="25%"></a>
         <button class="navbar-toggler text-dark" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="fa fa-bars"></span> Menu
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a href="../index.php" class="nav-link">Αρχική</a></li>
+            <li class="nav-item"><a href="../../index.php" class="nav-link">Αρχική</a></li>
             <li class="nav-item">
               <div class="dropdown">
                 <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Το Δίκτυο</a>
           <div class="dropdown-menu">
-                  <a class="dropdown-item" href="../network/info.php">Πληροφορίες και Χάρτης</a>
-                  <a class="dropdown-item" href="../network/journey_planner.php">Σχεδιασμός Διαδρομής</a>
-                  <a class="dropdown-item" href="../network/status.php">Κατάσταση Δικτύου</a>
+                  <a class="dropdown-item" href="../../network/info.php">Πληροφορίες και Χάρτης</a>
+                  <a class="dropdown-item" href="../../network/journey_planner.php">Σχεδιασμός Διαδρομής</a>
+                  <a class="dropdown-item" href="../../network/status.php">Κατάσταση Δικτύου</a>
                 </div>
               </div>
             </li>
@@ -65,8 +65,8 @@ HTML/CSS by: Maria Karamina (sdi1600059)
               <div class="dropdown">
                 <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Εισιτήρια</a>
           <div class="dropdown-menu">
-                  <a class="dropdown-item" href="../tickets/info.php">Πληροφορίες Εισιτηρίων</a>
-                  <a class="dropdown-item" href="../tickets/buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων</a>
+                  <a class="dropdown-item" href="../../tickets/info.php">Πληροφορίες Εισιτηρίων</a>
+                  <a class="dropdown-item" href="../../tickets/buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων</a>
                 </div>
               </div>
           </li>
@@ -74,10 +74,10 @@ HTML/CSS by: Maria Karamina (sdi1600059)
               <div class="dropdown">
                 <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Επιβάτες</a>
           <div class="dropdown-menu">
-                  <a class="dropdown-item" href="../passengers/lost_and_found.php">Απολεσθέντα</a>
-                  <a class="dropdown-item" href="../passengers/amea.php">ΆμεΑ</a> <!--TODO: correct translation :p -->
-                  <a class="dropdown-item" href="../passengers/complaints.php">Υποβολή Παραπόνων</a>
-                  <a class="dropdown-item" href="../passengers/help.php">Βοήθεια</a>
+                  <a class="dropdown-item" href="../../passengers/lost_and_found.php">Απολεσθέντα</a>
+                  <a class="dropdown-item" href="../../passengers/amea.php">ΆμεΑ</a> <!--TODO: correct translation :p -->
+                  <a class="dropdown-item" href="../../passengers/complaints.php">Υποβολή Παραπόνων</a>
+                  <a class="dropdown-item" href="../../passengers/help.php">Βοήθεια</a>
                 </div>
               </div>
           </li>
@@ -85,10 +85,10 @@ HTML/CSS by: Maria Karamina (sdi1600059)
               <div class="dropdown">
                 <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Εταιρία</a>
           <div class="dropdown-menu">
-                  <a class="dropdown-item" href="../company/info.php">Πληροφορίες Ομίλου</a>
-                  <a class="dropdown-item" href="../company/contact_details.php">Στοιχεία Επικοινωνίας</a>
-                  <a class="dropdown-item" href="../company/competitions.php">Διαγωνισμοί</a> <!--TODO: correct translation :p -->
-                  <a class="dropdown-item" href="../company/news.php">Νέα</a>
+                  <a class="dropdown-item" href="../../company/info.php">Πληροφορίες Ομίλου</a>
+                  <a class="dropdown-item" href="../../company/contact_details.php">Στοιχεία Επικοινωνίας</a>
+                  <a class="dropdown-item" href="../../company/competitions.php">Διαγωνισμοί</a> <!--TODO: correct translation :p -->
+                  <a class="dropdown-item" href="../../company/news.php">Νέα</a>
                 </div>
               </div>
           </li>
@@ -99,14 +99,14 @@ HTML/CSS by: Maria Karamina (sdi1600059)
                   <a class="dropdown-toggle nav-link user-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username'];?></a>
                   <div class="dropdown-menu">
                     <a class="dropdown-item disabled" href="#" disabled><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name'];?></a>
-                    <a class="dropdown-item" href="../account/profile.php">Προβολή Προφίλ</a>
-                    <a class="dropdown-item" href="../account/logout_script.php">Αποσύνδεση</a>
+                    <a class="dropdown-item" href="../../account/profile.php">Προβολή Προφίλ</a>
+                    <a class="dropdown-item" href="../../account/logout_script.php">Αποσύνδεση</a>
                   </div>
                 </div>
             <?php
               }
               else {
-                echo '<a href="../account/login.php" class="nav-link user-button">Σύνδεση</a>';
+                echo '<a href="../../account/login.php" class="nav-link user-button">Σύνδεση</a>';
               }
             ?>
           </li>
@@ -118,9 +118,9 @@ HTML/CSS by: Maria Karamina (sdi1600059)
 
     <div class="container crumbs-top">
       <p class="breadcrumbs">
-        <span class="mr-2"><a href="../index.php">Αρχική <i class="fa fa-angle-right"></i></a></span> 
+        <span class="mr-2"><a href="../../index.php">Αρχική <i class="fa fa-angle-right"></i></a></span> 
         <span class="mr-2">Εισιτήρια <i class="fa fa-angle-right"></i></span> 
-        <span class="mr-2"><a href="buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων <i class="fa fa-angle-right"></i></a></span> 
+        <span class="mr-2"><a href="../buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων <i class="fa fa-angle-right"></i></a></span> 
         <span class="mr-2">Έκδοση Κάρτας <i class="fa fa-angle-right"></i></span>
       </p>
     </div>
@@ -288,7 +288,7 @@ HTML/CSS by: Maria Karamina (sdi1600059)
                     <th>Τιμή</th>
                   </tr>
                 </table>
-                <script src="../js/get-selected-products.js"></script>
+                <script src="../../js/get-selected-products.js"></script>
               </div>
               <div class="col-md-3">
                 <p>Τελικό ποσό προς πληρωμή: <span id="total-price">€</span></p>
@@ -355,9 +355,9 @@ HTML/CSS by: Maria Karamina (sdi1600059)
             <div class="ftco-footer-widget mb-4 ml-md-5">
               <h2 class="ftco-heading-2">Δίκτυο</h2>
               <ul class="list-unstyled">
-                <li><a href="../network/info.php" class="py-2 d-block">Πληροφορίες και Χάρτης</a></li>
-                <li><a href="../network/journey_planner.php" class="py-2 d-block">Σχεδιασμός Διαδρομής</a></li>
-                <li><a href="../network/status.php" class="py-2 d-block">Κατάσταση Δικτύου</a></li>
+                <li><a href="../../network/info.php" class="py-2 d-block">Πληροφορίες και Χάρτης</a></li>
+                <li><a href="../../network/journey_planner.php" class="py-2 d-block">Σχεδιασμός Διαδρομής</a></li>
+                <li><a href="../../network/status.php" class="py-2 d-block">Κατάσταση Δικτύου</a></li>
               </ul>
             </div>
           </div>
@@ -365,8 +365,8 @@ HTML/CSS by: Maria Karamina (sdi1600059)
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Εισιτήρια</h2>
               <ul class="list-unstyled">
-                <li><a href="../tickets/info.php" class="py-2 d-block">Πληροφορίες Εισιτηρίων</a></li>
-                <li><a href="../tickets/buy_online.php" class="py-2 d-block">Ηλεκτρονική Αγορά Εισιτηρίων</a></li>
+                <li><a href="../../tickets/info.php" class="py-2 d-block">Πληροφορίες Εισιτηρίων</a></li>
+                <li><a href="../../tickets/buy_online.php" class="py-2 d-block">Ηλεκτρονική Αγορά Εισιτηρίων</a></li>
               </ul>
             </div>
           </div>
@@ -374,10 +374,10 @@ HTML/CSS by: Maria Karamina (sdi1600059)
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Επιβάτες</h2>
               <ul class="list-unstyled">
-                <li><a href="../passengers/lost_and_found.php" class="py-2 d-block">Απολεσθέντα</a></li>
-                <li><a href="../passengers/amea.php" class="py-2 d-block">ΆμεΑ</a></li>
-                <li><a href="../passengers/complaints.php" class="py-2 d-block">Υποβολη Παραπόνων</a></li>
-                <li><a href="../passengers/help.php" class="py-2 d-block">Βοήθεια</a></li>
+                <li><a href="../../passengers/lost_and_found.php" class="py-2 d-block">Απολεσθέντα</a></li>
+                <li><a href="../../passengers/amea.php" class="py-2 d-block">ΆμεΑ</a></li>
+                <li><a href="../../passengers/complaints.php" class="py-2 d-block">Υποβολη Παραπόνων</a></li>
+                <li><a href="../../passengers/help.php" class="py-2 d-block">Βοήθεια</a></li>
               </ul>
             </div>
           </div>
@@ -399,26 +399,26 @@ HTML/CSS by: Maria Karamina (sdi1600059)
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="../js/popper.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
-  <script src="../js/jquery.easing.1.3.js"></script>
-  <script src="../js/jquery.waypoints.min.js"></script>
-  <script src="../js/jquery.stellar.min.js"></script>
-  <script src="../js/owl.carousel.min.js"></script>
-  <script src="../js/jquery.magnific-popup.min.js"></script>
-  <script src="../js/aos.js"></script>
-  <script src="../js/jquery.animateNumber.min.js"></script>
-  <script src="../js/bootstrap-datepicker.js"></script>
-  <script src="../js/jquery.timepicker.min.js"></script>
-  <script src="../js/scrollax.min.js"></script>
+  <script src="../../js/jquery.min.js"></script>
+  <script src="../../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../../js/popper.min.js"></script>
+  <script src="../../js/bootstrap.min.js"></script>
+  <script src="../../js/jquery.easing.1.3.js"></script>
+  <script src="../../js/jquery.waypoints.min.js"></script>
+  <script src="../../js/jquery.stellar.min.js"></script>
+  <script src="../../js/owl.carousel.min.js"></script>
+  <script src="../../js/jquery.magnific-popup.min.js"></script>
+  <script src="../../js/aos.js"></script>
+  <script src="../../js/jquery.animateNumber.min.js"></script>
+  <script src="../../js/bootstrap-datepicker.js"></script>
+  <script src="../../js/jquery.timepicker.min.js"></script>
+  <script src="../../js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="../js/google-map.js"></script>
-  <script src="../js/main.js"></script>
+  <script src="../../js/google-map.js"></script>
+  <script src="../../js/main.js"></script>
 
-  <script src="../js/buy-card-steps.js"></script>
-  <script src="../js/add-products.js"></script>
+  <script src="../../js/buy-card-steps.js"></script>
+  <script src="../../js/add-products.js"></script>
     
   </body>
 </html>
