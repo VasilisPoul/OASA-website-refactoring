4763118a1625e40b1183bd731d127418c92d6805
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2020-01-09 00:53:59 +0200

    Add files via upload

---
+++ oasa/tickets/recharge_card/confirm_recharge.php
@@ -0,0 +1,234 @@
+<!-- 
+
+HTML/CSS by: Giorgos Koursiounis (sdi1600077)
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ΟΑΣΑ - Επαναφόρτιση Κάρτας</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="../../images/favicon.ico" type="image/ico">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../../css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    
+    <link rel="stylesheet" href="../../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../css/magnific-popup.css">
+
+    <link rel="stylesheet" href="../../css/aos.css">
+
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
+
+    <link rel="stylesheet" href="../../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../../css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="../../css/flaticon.css">
+    <link rel="stylesheet" href="../../css/icomoon.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/additional.css">
+  </head>
+  <body>
+
+    <?php include 'confirm_recharge_script.php' ?>
+    
+   <nav class="navbar navbar-expand-lg navbar-light ftco_navbar bg-dark ftco-navbar-light navbar-color" id="ftco-navbar">
+      <div class="container">
+        <a href="../../index.php"><img src="../../images/oasa_logo_transparent.png" alt="logo" width="25%"></a>
+        <button class="navbar-toggler text-dark" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fa fa-bars"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="../../index.php" class="nav-link">Αρχική</a></li>
+            <li class="nav-item">
+              <div class="dropdown">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Το Δίκτυο</a>
+          <div class="dropdown-menu">
+                  <a class="dropdown-item" href="../../network/info.php">Πληροφορίες και Χάρτης</a>
+                  <a class="dropdown-item" href="../../network/journey_planner.php">Σχεδιασμός Διαδρομής</a>
+                  <a class="dropdown-item" href="../../network/status.php">Κατάσταση Δικτύου</a>
+                </div>
+              </div>
+            </li>
+            <li class="nav-item active">
+              <div class="dropdown">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Εισιτήρια</a>
+          <div class="dropdown-menu">
+                  <a class="dropdown-item" href="../../tickets/info.php">Πληροφορίες Εισιτηρίων</a>
+                  <a class="dropdown-item" href="../../tickets/buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων</a>
+                </div>
+              </div>
+          </li>
+            <li class="nav-item">
+              <div class="dropdown">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Επιβάτες</a>
+          <div class="dropdown-menu">
+                  <a class="dropdown-item" href="../../passengers/lost_and_found.php">Απολεσθέντα</a>
+                  <a class="dropdown-item" href="../../passengers/amea.php">ΆμεΑ</a> <!--TODO: correct translation :p -->
+                  <a class="dropdown-item" href="../../passengers/complaints.php">Υποβολή Παραπόνων</a>
+                  <a class="dropdown-item" href="../../passengers/help.php">Βοήθεια</a>
+                </div>
+              </div>
+          </li>
+            <li class="nav-item">
+              <div class="dropdown">
+                <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Εταιρία</a>
+          <div class="dropdown-menu">
+                  <a class="dropdown-item" href="../../company/info.php">Πληροφορίες Ομίλου</a>
+                  <a class="dropdown-item" href="../../company/contact_details.php">Στοιχεία Επικοινωνίας</a>
+                  <a class="dropdown-item" href="../../company/competitions.php">Διαγωνισμοί</a> <!--TODO: correct translation :p -->
+                  <a class="dropdown-item" href="../../company/news.php">Νέα</a>
+                </div>
+              </div>
+          </li>
+          <li class="nav-item">
+            <?php
+              if(isset($_SESSION['loggedin']) && ($_SESSION['loggedin'])){ ?>
+                <div class="dropdown">
+                  <a class="dropdown-toggle nav-link user-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username'];?></a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item disabled" href="#" disabled><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name'];?></a>
+                    <a class="dropdown-item" href="../../account/profile.php">Προβολή Προφίλ</a>
+                    <a class="dropdown-item" href="../../account/logout_script.php">Αποσύνδεση</a>
+                  </div>
+                </div>
+            <?php
+              }
+              else {
+                echo '<a href="../../account/login.php" class="nav-link user-button">Σύνδεση</a>';
+              }
+            ?>
+          </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- END nav -->
+    
+    <div class="container crumbs-top">
+      <p class="breadcrumbs">
+        <span class="mr-2"><a href="../../index.php">Αρχική <i class="fa fa-angle-right"></i></a></span> 
+        <span class="mr-2">Εισιτήρια <i class="fa fa-angle-right"></i></span> 
+        <span class="mr-2"><a href="../buy_online.php">Ηλεκτρονική Αγορά Εισιτηρίων <i class="fa fa-angle-right"></i></a></span>
+        <span class="mr-2"> Επαναφόρτιση Κάρτας <i class="fa fa-angle-right"></i></span>
+      </p>
+    </div>
+
+    <section class="bg-light front-page-text page-header">
+      <div class="container">
+        <div class="row no-gutters align-items-end justify-content-center text-center">
+          <div class="col-md-9 ftco-animate pb-5">
+            
+            <h1>Επαναφόρτιση Κάρτας</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section ftco-no-pt bg-light">
+      <?php echo $email_err; ?>
+      <?php echo $idcard_err; ?>
+      <?php echo $idticket_category_err; ?>
+      <?php echo $message; ?>
+
+      <div style="overflow:auto;">
+        <div style="float:right;">
+          <a href="../../index.php" class="btn btn-primary mr-4">Επιστροφή στην Αρχική Σελίδα</a>
+        </div>
+      </div>
+    </section>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2>ΟΑΣΑ - Οργανισμός Αστικών Συγκοινωνιών Αθηνών Α.Ε.</h2>
+              <p>
+                Εταιρίες Ομίλου
+                <ul class="list-unstyled">
+                  <li><a href="http://www.osy.gr" target="_blank">ΟΣΥ - Οδικές Συγκοινωνίες Α.Ε.</a></li>
+                  <li><a href="http://www.stasy.gr" target="_blank">ΣΤΑΣΥ - Σταθερές Συγκοινωνίες Α.Ε.</a></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Δίκτυο</h2>
+              <ul class="list-unstyled">
+                <li><a href="../../network/info.php" class="py-2 d-block">Πληροφορίες και Χάρτης</a></li>
+                <li><a href="../../network/journey_planner.php" class="py-2 d-block">Σχεδιασμός Διαδρομής</a></li>
+                <li><a href="../../network/status.php" class="py-2 d-block">Κατάσταση Δικτύου</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Εισιτήρια</h2>
+              <ul class="list-unstyled">
+                <li><a href="../../tickets/info.php" class="py-2 d-block">Πληροφορίες Εισιτηρίων</a></li>
+                <li><a href="../../tickets/buy_online.php" class="py-2 d-block">Ηλεκτρονική Αγορά Εισιτηρίων</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Επιβάτες</h2>
+              <ul class="list-unstyled">
+                <li><a href="../../passengers/lost_and_found.php" class="py-2 d-block">Απολεσθέντα</a></li>
+                <li><a href="../../passengers/amea.php" class="py-2 d-block">ΆμεΑ</a></li>
+                <li><a href="../../passengers/complaints.php" class="py-2 d-block">Υποβολη Παραπόνων</a></li>
+                <li><a href="../../passengers/help.php" class="py-2 d-block">Βοήθεια</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="../../js/jquery.min.js"></script>
+  <script src="../../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../../js/popper.min.js"></script>
+  <script src="../../js/bootstrap.min.js"></script>
+  <script src="../../js/jquery.easing.1.3.js"></script>
+  <script src="../../js/jquery.waypoints.min.js"></script>
+  <script src="../../js/jquery.stellar.min.js"></script>
+  <script src="../../js/owl.carousel.min.js"></script>
+  <script src="../../js/jquery.magnific-popup.min.js"></script>
+  <script src="../../js/aos.js"></script>
+  <script src="../../js/jquery.animateNumber.min.js"></script>
+  <script src="../../js/bootstrap-datepicker.js"></script>
+  <script src="../../js/jquery.timepicker.min.js"></script>
+  <script src="../../js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../../js/google-map.js"></script>
+  <script src="../../js/main.js"></script>
+    
+  </body>
+</html>
---
+++ oasa/tickets/recharge_card/confirm_recharge_script.php
@@ -0,0 +1,111 @@
+<?php
+
+//
+// PHP script by: Giorgos Koursiounis (sdi1600077)
+//
+
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$servername = "localhost";
+$server_username = "user";
+$server_password = "password";
+$dbname = "oasa";
+
+$date = date("Y-m-d");
+$total_amount = 0.0;
+
+$email = $idcard = $pin = $idticket_category = $message = "";
+$email_err = $idcard_err = $idticket_category_err = $pin_err = "";
+
+
+if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
+
+  if(isset($_SESSION['loggedin'])){
+    $content = "Αγαπητέ/ή ". $_SESSION['first_name']. " " . $_SESSION['last_name'] . ",\n   Πραγματοποιήθηκε την $date online επαναφόρτιση κάρτας με τις παρακάτω λεπτομέρειες:\n<ul>";
+  }
+  else{
+    $content = "Αγαπητέ/ή χρήστη,\n   Πραγματοποιήθηκε την $date online επαναφόρτιση κάρτας με τις παρακάτω λεπτομέρειες:\n<ul>";
+  }
+
+  $mailheader = "From: e-tickets@oasa.gr \r\nContent-Type: text/plain; charset=UTF-8 \r\n"; 
+
+
+  $conn = new mysqli($servername, $server_username, $server_password, $dbname);
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+
+  if(empty($_POST["email"])){
+    $email_err = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Απαιτείται email</div>";
+  } 
+  else{
+    $email = $_POST["email"];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_err = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Λανθασμένη μορφή email</div>";
+    }
+  }
+
+  if(empty($_POST["idcard"])){
+    $idcard_err = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Απαιτείται κωδικός κάρτας</div>";
+  } 
+  else{
+    $idcard = $_POST["idcard"];
+  }
+
+  if(empty($_POST["idticket_category"])){
+    $idticket_category_err = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Απαιτείται κωδικός κατηγορίας</div>";
+  } 
+  else{
+    $idticket_category = $_POST["idticket_category"];
+  }
+
+  if(empty($_POST["pin"])){
+    $pin_err = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Απαιτείται κωδικός PIN</div>";
+  } 
+  else{
+    $pin = $_POST["pin"];
+  }
+
+
+  if(empty($email_err) && empty($idticket_category_err) && empty($idcard_err) && empty($pin_err)){
+
+    $sql = "SELECT c.date, c.pin, tc.name, tc.price FROM card c, ticket_category tc WHERE c.idcard = $idcard AND c.idticket_category = tc.idticket_category";
+    $result = $conn->query($sql);
+
+    if(!empty($result) && $result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+
+        if($row["pin"] == $pin){
+          
+          $sql1 = "UPDATE card SET date = \"$date\", idticket_category = \"$idticket_category\", expired = 0 WHERE idcard = \"$idcard\"";
+
+          if($conn->query($sql1) === TRUE){
+            $content .= "<li>Κωδικός κάρτας: " . $idcard . "\n Τύπος: " . $row["name"] . "\n Τιμή: " . $row["price"] . " €</li>\n</ul>Συνολικό ποσό πληρωμής: <strong>" . $row["price"] . " €</strong>\n Με εκτίμηση,\n Οργανισμός Αστικών Συγκοινωνιών Αθηνών (ΟΑΣΑ)\n www.oasa.gr";
+
+            if(mail($email, "OASA tickets", $content, $mailheader)){
+              $message = "<div class=\"alert alert-success\"><strong>Επιτυχία!</strong> Η συναλλαγή ολοκληρώθηκε! Σας έχει αποσταλεί σχετικό email</div>";
+            }
+            else{
+              $message = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Η αποστολή email απέτυχε και θα επαναληφθεί το συντομότερο δυνατό <br /> SMTP Server Error </div>";
+            }
+          }
+        }
+        else{
+          $error = "Ο κωδικός PIN δεν είναι σωστός";
+        }
+      }
+    }
+    else{
+      $error = "Η κάρτα δεν βρέθηκε";
+    }
+  }
+  $conn->close();
+
+}
+else{
+  $message = "<div class=\"alert alert-danger\"><strong>Αποτυχία!</strong> Δεν έχετε συμπληρώσει τη φόρμα επαναφόρτισης κάρτας</div>";
+}
+?>
