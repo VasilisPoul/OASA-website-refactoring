164531d4bbe9568a2fc8f4d4f8ecaef3ea131f20
Maria <maria.karamina@gmail.com>
2020-01-11 20:30:17 +0200

    Line info

---
+++ oasa/js/line_info-inputs.js
@@ -0,0 +1,26 @@
+function inputToUrl(param, value) {
+	var url = new URL(window.location);
+	url.searchParams.set(param, value);
+	window.location.href = url.toString();	
+}
+
+function urlToOutput() {
+	const url = new URL(window.location);
+	if(url.searchParams == "") {
+		return;
+	}
+
+	var param;
+	if(url.pathname.includes("lines.php")) {
+		param = url.searchParams.get("line");
+	}
+	else if(url.pathname.includes("stations.php")) {
+		param = url.searchParams.get("station");
+	}
+	else {
+		param = url.searchParams.get("area");
+	}
+
+	var output = document.getElementById("output");
+	output.innerHTML = "Looking for: " + param;
+}
