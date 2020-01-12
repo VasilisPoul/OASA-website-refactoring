f8177eda4346435e18a12998f013c642f4f73bde
George Koursiounis <36711755+gkoursiounis@users.noreply.github.com>
2020-01-12 14:59:44 +0200

    Add files via upload

--- oasa/network/journey_planner_script.php
+++ oasa/network/journey_planner_script.php
@@ -16,6 +16,9 @@ $dbname = "oasa";
 $departure = $arrival = "";
 $departure_err = $arrival_err = "";
 
+$coordinates_str = "[";
+$message = "";
+
 if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "GET"){
 
   //check if departure point is given
@@ -41,19 +44,60 @@ if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "GET"){
   }
 
   //find all coordinates of all stations
-  $sql = "SELECT * FROM station";
+  $sql = "SELECT s.name, s.latitude, s.longitude, cl.colour FROM station s, line_has_station ls, line l, colour cl WHERE s.idstation = ls.idstation AND ls.idline = l.idline AND l.idcolour = cl.idcolour";
   $result = $conn->query($sql);
+
+  $coordinates = array();
 
   if(!empty($result) && $result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-      
-      if($departure == "Ομόνοια" && $arrival == "Ευαγγελισμός")
-      {
-        
-      }
+      $coordinates[$row["name"]] = [$row["latitude"], $row["longitude"], $row["colour"]];
     }
+  }
+
+  if(strtolower($departure) == "ομονοια" && strtolower($arrival) == "ευαγγελισμος")
+  {
+    //first alternative root
+    $coordinates_str .= "[{{lat: " . $coordinates["ΟΜΟΝΟΙΑ"][0] . " , lng: " . $coordinates["ΟΜΟΝΟΙΑ"][1] . "},{colour: " . $coordinates["ΟΜΟΝΟΙΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][0] . " , lng: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][1] . "},{colour: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΣΥΝΤΑΓΜΑ"][0] . " , lng: " . $coordinates["ΣΥΝΤΑΓΜΑ"][1] . "},{colour: " . $coordinates["ΣΥΝΤΑΓΜΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][0] . " , lng: " . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][1] . "},{colour:"  . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][2] . "}}]";
+
+    //second alternative root
+    $coordinates_str .= "[{{lat: " . $coordinates["ΟΜΟΝΟΙΑ"][0] . " , lng: " . $coordinates["ΟΜΟΝΟΙΑ"][1] . "},{colour: " . $coordinates["ΟΜΟΝΟΙΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][0] . " , lng: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][1] . "},{colour: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΣΥΝΤΑΓΜΑ"][0] . " , lng: " . $coordinates["ΣΥΝΤΑΓΜΑ"][1] . "},{colour: " . $coordinates["ΣΥΝΤΑΓΜΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][0] . " , lng: " . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][1] . "},{colour:"  . $coordinates["ΕΥΑΓΓΕΛΙΣΜΟΣ"][2] . "}}]";
+  }
+  else if(strtolower($departure) == "κεραμεικος" && strtolower($arrival) == "πανεπιστημιο")
+  {
+    //first alternative root
+    $coordinates_str .= "[{{lat: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][0] . " , lng: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][1] . "},{colour: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][0] . " , lng: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][1] . "},{colour: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΣΥΝΤΑΓΜΑ"][0] . " , lng: " . $coordinates["ΣΥΝΤΑΓΜΑ"][1] . "},{colour: " . $coordinates["ΣΥΝΤΑΓΜΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][0] . " , lng: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][1] . "},{colour:"  . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][2] . "}}]";
+
+    //second alternative root
+    $coordinates_str .= "[{{lat: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][0] . " , lng: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][1] . "},{colour: " . $coordinates["ΚΕΡΑΜΕΙΚΟΣ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][0] . " , lng: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][1] . "},{colour: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΟΜΟΝΟΙΑ"][0] . " , lng: " . $coordinates["ΟΜΟΝΟΙΑ"][1] . "},{colour: " . $coordinates["ΟΜΟΝΟΙΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][0] . " , lng: " . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][1] . "},{colour:"  . $coordinates["ΠΑΝΕΠΙΣΤΗΜΙΟ"][2] . "}}]";
+  }
+  else if(strtolower($departure) == "βικτωρια" && strtolower($arrival) == "θησειο")
+  {
+    $coordinates_str .= "[{{lat: " . $coordinates["ΒΙΚΤΩΡΙΑ"][0] . " , lng: " . $coordinates["ΒΙΚΤΩΡΙΑ"][1] . "},{colour: " . $coordinates["ΒΙΚΤΩΡΙΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΟΜΟΝΟΙΑ"][0] . " , lng: " . $coordinates["ΟΜΟΝΟΙΑ"][1] . "},{colour: " . $coordinates["ΟΜΟΝΟΙΑ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][0] . " , lng: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][1] . "},{colour: " . $coordinates["ΜΟΝΑΣΤΗΡΑΚΙ"][2] . "}}";
+    $coordinates_str .= "{{lat: " . $coordinates["ΘΗΣΕΙΟ"][0] . " , lng: " . $coordinates["ΘΗΣΕΙΟ"][1] . "},{colour:"  . $coordinates["ΘΗΣΕΙΟ"][2] . "}}]";
+  }
+  else
+  {
+    $message = "Λυπούμαστε η διαδρομή δεν βρέθηκε!";
   }
 
   $conn->close();
 }
+
+$coordinates_str .= "]";
+
 ?>
