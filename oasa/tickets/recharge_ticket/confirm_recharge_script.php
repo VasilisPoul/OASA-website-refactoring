f3fe251cb57321c1e06765207f2e73359b4ae6db
Maria <maria.karamina@gmail.com>
2020-01-14 21:31:11 +0200

    Schema changes

--- oasa/tickets/recharge_ticket/confirm_recharge_script.php
+++ oasa/tickets/recharge_ticket/confirm_recharge_script.php
@@ -29,7 +29,7 @@ if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
     $content = "Αγαπητέ/ή χρήστη,\n   Πραγματοποιήθηκε την $date online επαναφόρτιση εισιτηρίου με τις παρακάτω λεπτομέρειες:\n<ul>";
   }
 
-  $mailheader = "From: e-tickets@sdi1600077.gr \r\nContent-Type: text/plain; charset=UTF-8 \r\n"; 
+  $mailheader = "From: e-tickets@oasa.gr \r\nContent-Type: text/plain; charset=UTF-8 \r\n"; 
 
 
   $conn = new mysqli($servername, $server_username, $server_password, $dbname);
@@ -82,7 +82,7 @@ if(!empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
         die("Error: " . $sql . "<br>" . $conn->error);
       }
 
-      $content .= "</ul>Συνολικό ποσό πληρωμής: <strong>" . $total_amount . " €</strong>\n Με εκτίμηση,\n Οργανισμός Αστικών Συγκοινωνιών Αθηνών (ΟΑΣΑ)\n www.sdi1600077.gr";
+      $content .= "</ul>Συνολικό ποσό πληρωμής: <strong>" . $total_amount . " €</strong>\n Με εκτίμηση,\n Οργανισμός Αστικών Συγκοινωνιών Αθηνών (ΟΑΣΑ)\n www.oasa.gr";
 
       if(mail($email, "OASA tickets", $content, $mailheader)){
         $message = "<div class=\"alert alert-success\"><strong>Επιτυχία!</strong> Η συναλλαγή ολοκληρώθηκε! Σας έχει αποσταλεί σχετικό email</div>";
